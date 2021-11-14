2362786b83f12c346a41899ea2577c8c7b4272a3
Kip Yin <28321392+kipyin@users.noreply.github.com>
2021-11-15 07:33:44 +0800

    Add `SongResource`, representing a music sheet or a media file.

--- src/legoworship/legoworship.py
+++ src/legoworship/legoworship.py
@@ -1,8 +1,10 @@
 """Main module."""
 
 import csv
+import os
 from pathlib import Path
-from typing import ClassVar, List, Optional, Sequence, Type, TypeVar, Union
+from string import Template
+from typing import ClassVar, Dict, List, Optional, Sequence, Type, TypeVar, Union
 
 import attr
 from pypinyin import lazy_pinyin
@@ -12,6 +14,18 @@ from pypinyin import lazy_pinyin
 PINYIN_ADJUSTMENTS = {ord("祢"): "nǐ,mí"}
 
 
+SONG_PAGE_TEMPLATE = Template(
+    """---
+layout: song
+title: $title
+alternative_titles: $alternative_titles
+lyricist: $lyricist
+composer: $composer
+---
+
+"""
+)
+
 T = TypeVar("T", bound="Song")
 
 
@@ -20,10 +34,13 @@ class Song:
     """A song."""
 
     title: str
+    alternative_titles: Optional[List[str]] = None
     original_key: Optional[str] = None
     bpm: Optional[int] = None
     lyricist: Optional[str] = None
     composer: Optional[str] = None
+    resources: Optional[List["SongResource"]] = None
+    lyrics: Optional[str] = None
 
     @property
     def pinyin_title(self: T) -> List[str]:
@@ -41,6 +58,75 @@ class Song:
             A list of each character's pinyin
         """
         return [pinyin.title() for pinyin in lazy_pinyin(self.title.split(" "))]
+
+    @property
+    def alternative_title_string(self: T) -> str:
+        """Combine all alternative titles into one string."""
+        return " / ".join(self.alternative_titles)
+
+    def match_file(self: T, filename: str, extensions: List[str]) -> bool:
+        """Match if a filename contains the searched title or one of the extensions."""
+        if self.title in filename:
+            for extension in extensions:
+                if filename.endswith(extension):
+                    return True
+        return False
+
+    def find_resources(
+        self: T, resource_type: str, library: str, extension: Optional[str] = None
+    ) -> bool:
+        """Find and build song resources.
+
+        Walk through each file in `library` and compare each song title with the filename,
+        if there is a match, append it to a result list.
+
+        Args:
+            resource_type: "sheet" or "media".
+            library: the path to search in.
+            extension: match the extension if provided, otherwise a list of default
+                extensions will be matched based on `resource_type`.
+
+        Returns:
+            True if resources are found, otherwise False
+
+        Raises:
+            ValueError: if `resource_type` does not match any of the default
+                resource types.
+        """
+        if not extension and resource_type not in SongResource.EXTENSIONS:
+            raise ValueError(f"Resource type of '{resource_type}' is not supported.")
+        self.resources = [] if self.resources is None else self.resources
+        search_extensions = [extension if extension else SongResource.EXTENSIONS[resource_type]]
+        for root, _, files in os.walk(library):
+            for file in files:
+                if self.match_file(filename=file, extensions=search_extensions):
+                    self.resources.append(
+                        SongResource(
+                            song=self,
+                            resource_type=resource_type,
+                            location=os.path.join(root, file),
+                        )
+                    )
+        if self.resources:
+            return True
+        else:
+            return False
+
+    def check_page_exists(self: T, page_dir: str) -> bool:
+        """Check if the song's page exists in `page_dir`."""
+        # TODO: first should check if `page_dir` exists.
+        if not os.path.isdir(page_dir):
+            raise ValueError(f"{page_dir} is not a valid page directory.")
+        return os.path.isfile(os.path.join(page_dir, self.title, ".md"))
+
+    def create_page(self: T, page_dir: str, quiet: bool = False) -> bool:
+        """Create a song's page if it does not exist."""
+        if self.check_page_exists(page_dir):
+            if quiet:
+                return False
+            else:
+                raise ValueError(f"Song page for {self.title} already exists.")
+        os.mkdir(os.path.join(page_dir, self.title, ".md"))
 
 
 S = TypeVar("S", bound="SongList")
@@ -187,8 +273,85 @@ class SongList:
         return cls(name=Path(csv_file_path).name, songs=songs)
 
 
+@attr.s(auto_attribs=True)
+class SongResource:
+    """A music sheet or a media file for a song.
+
+    Args:
+        song: A `Song` instance linked to this resource.
+        resource_type: either "sheet" or "media".
+        location: the path pointing to the resource file.
+        bpm: the beats per minutes (tempo) of the resource.
+        key: the musical key of the resource.
+        artist: the artist of the resource.
+        album: the album of the resource.
+    """
+
+    EXTENSIONS: ClassVar[Dict[str, List[str]]] = {
+        "sheet": [".png", ".pdf"],
+        "media": [".mp3", ".m4a", ".wav"],
+    }
+
+    song: Song
+    resource_type: str  # "sheet" or "media"
+    location: str
+    bpm: Optional[int] = None
+    key: Optional[str] = None
+    performer: Optional[str] = None
+    album: Optional[str] = None
+
+
+def find(title: str, what: str, path: str) -> Union[List[str], bool]:
+    """Find a song in path."""
+    if what not in ["sheet", "media"]:
+        raise ValueError(f"Only `sheet` and `media` are supported, not {what}.")
+    results = []
+    for dirpath, _, files in os.walk(path):
+        for filename in files:
+            if what == "sheet":
+                if title in filename and filename.endswith(".png") and "TINY" in dirpath:
+                    found_path = os.path.join(dirpath, filename)
+                    print(found_path)
+                    results.append(found_path)
+            elif what == "media":
+                if title in filename and filename.endswith(".mp3"):
+                    found_path = os.path.join(dirpath, filename)
+                    print(found_path)
+                    results.append(found_path)
+            else:
+                pass
+    if not results:
+        print(f"no results found for {title} in {path}")
+        return False
+    return results
+
+
+def find_multiple(what: str, path: str, song_list: SongList) -> Union[List[str], bool]:
+    """Find multiple _songs in a song list."""
+    results = []
+    for dirpath, _, files in os.walk(path):
+        for filename in files:
+            for song in song_list.songs:
+                if song.title in filename:
+                    if (what == "sheet" and filename.endswith(".png") and "TINY" in dirpath) or (
+                        what == "media" and filename.endswith(".mp3")
+                    ):
+                        found_path = os.path.join(dirpath, filename)
+                        print(song.title)
+                        print(found_path)
+                        results.append(found_path)
+    return results
+
+
 if __name__ == "__main__":
-    song_list = SongList.from_csv("docs/_data/songs.csv", legacy=True)
-    print(song_list)
-    song_list.songs.append(Song(title="爱赢了", original_key="C"))
-    song_list.sort(by="title", legacy=False).export_csv(to="docs/_data/all_songs.csv")
+    import subprocess  # noqa
+    from pprint import pprint
+
+    SHEET_LIB = "/Users/kip/Mercury/3.Ecclasia/4. 灵栖清泉"
+    MUSIC_LIB = "/Volumes/music"
+    song_list = SongList.from_csv(csv_file_path="docs/_data/all_songs.csv", legacy=False)
+    song = song_list.songs[-2]
+    pprint(f"Song: {song.title}")
+    song.find_resources("sheet", library=SHEET_LIB, extension=".pdf")
+    pprint([x.location for x in song.resources])
+    subprocess.Popen('open "{song.resources[0].location}"', shell=True)  # noqa
