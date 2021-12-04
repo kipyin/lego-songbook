656292fb1e98b9eae5616a5926dd6d00afdb62cb
Kip Yin <28321392+kipyin@users.noreply.github.com>
2021-12-04 10:36:45 +0800

    Bump version: 2021.12.6 → 2021.12.7

--- src/legoworship/__init__.py
+++ src/legoworship/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Kip Yin"""
 __email__ = "28321392+kipyin@users.noreply.github.com"
-__version__ = "2021.12.6"
+__version__ = "2021.12.7"
 
 from .legoworship import Song, SongList  # noqa: F401
